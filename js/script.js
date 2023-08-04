cbd39d9eadf8439387c48b3f17064e29dd5e4a4a
ogabekgeldiyorov <ogabek31012005@gmail.com>
2023-08-04 18:16:47 +0500

    add index.html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var  x =12;
+var y=2;
+
+var z = x + y;
+console.log('yigindi'+" "+ z +" "+'ga teng');
+
+var z = x - y;
+console.log('ayirma'+" "+ z +" "+'ga teng');
+var z = x * y;
+console.log('kupaytma'+" "+ z +" "+'ga teng');
+var z = x / y;
+console.log('bulinuvchi'+" "+ z +" "+'ga teng');
